251b1e43bb85df175aedba3da5d4cce067559a4d
Paul Hedderly <paul+git@mjr.org>
2008-10-21 17:23:38 +0100

    Added a serializable proc routine so I can pass strings and get procs.

--- prh-ruby.rb
+++ prh-ruby.rb
@@ -28,3 +28,57 @@ class Net::SSH::Connection::Session
     return @r
   end
 end
+
+# This stolen from: http://www.rubyquiz.com/quiz38.html
+class SerializableProc
+  def self._load( proc_string )
+    new(proc_string)
+  end
+
+  def initialize( proc_string )
+    @code = proc_string
+    @proc = nil
+  end
+
+  def _dump( depth )
+    @code
+  end
+
+  def method_missing( method, *args )
+    if to_lambda.respond_to? method
+      @proc.send(method, *args)
+    else
+      super
+    end
+  end
+
+  def to_proc( )
+    return @proc unless @proc.nil?
+
+    if @code =~ /\A\s*(?:lambda|proc)(?:\s*\{|\s+do).*(?:\}|end)\s*\Z/
+    @proc = eval @code
+    elsif @code =~ /\A\s*(?:\{|do).*(?:\}|end)\s*\Z/
+    @proc = eval "Proc.new #{@code}"
+    else
+      @proc = eval "Proc.new { #{@code} }"
+    end
+  end
+
+  def to_lambda( )
+    return @proc unless @proc.nil?
+
+    if @code =~ /\A\s*(?:lambda|proc)(?:\s*\{|\s+do).*(?:\}|end)\s*\Z/
+    @proc = eval @code
+    elsif @code =~ /\A\s*(?:\{|do).*(?:\}|end)\s*\Z/
+    @proc = eval "lambda #{@code}"
+    else
+      @proc = eval "lambda { #{@code} }"
+    end
+  end
+
+  def to_yaml( )
+    @proc = nil
+    super
+  end
+end
+
