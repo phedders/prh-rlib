14b230cd392d5d4d3a04a00a78428d2a84d91ea7
Paul Hedderly <paul+git@mjr.org>
2008-10-30 13:04:49 +0000

    Simple one stop encrypt/decrypt routines - return nil on fail

--- prh-ruby.rb
+++ prh-ruby.rb
@@ -143,3 +143,24 @@ end
 def eon?(obj)
   not defined? obj or obj.nil? or obj.empty?
 end
+
+def simple_decrypt(iv,crypted,password)
+  crypt = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+  crypt.decrypt
+  crypt.key = Digest::SHA256.digest(password)
+  crypt.iv = iv
+  data = crypt.update(crypted)
+  data << crypt.final rescue nil
+end
+
+def simple_encrypt(iv,data,password)
+  crypted = {}
+  crypt = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+  crypt.encrypt
+  crypt.key = Digest::SHA256.digest(password)
+  crypt.iv = crypted[:iv] = iv ? iv : crypt.random_iv
+  crypted[:data] = crypt.update(data)
+  crypted[:data] << crypt.final
+  return crypted
+end
+
