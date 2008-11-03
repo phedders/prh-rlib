6d28f616672dddc52dd86530c93d975a582617a4
Paul Hedderly <paul+git@mjr.org>
2008-11-03 16:58:52 +0000

    tweeaks plus push_branch start

--- prh-ruby.rb
+++ prh-ruby.rb
@@ -112,32 +112,42 @@ def debug(level,*objects)
 end
 
 class Git::Base
+  # get_branch is short for "GET ME THAT BRANCH" - ie just do it, trounce
+  # anything in the way, make sure config is set to merge from origin and that
+  # origin gets merged
   def get_branch(branch,*opts)
-    debug 6, "get_branch #{branch} opts=#{opts}"
+    debug 7, "Git::get_branch #{branch} opts=#{opts}"
     local=self.branches.local.map{|l| l.full}.grep(Regexp.new("^#{branch}$"))
     remote=self.branches.remote.map{|l| l.full}.grep(Regexp.new("^origin/#{branch}$"))
     if local.any? and remote.any?
-      debug 7, "just checkout and update"
+      debug 7, "Git::get_branch just checkout and update"
       self.config("remote.#{branch}.remote", "origin")
       self.config("remote.#{branch}.merge", "refs/heads/#{branch}")
       self.checkout(branch)
-      self.pull
+      self.pull("origin",branch)
     elsif local.any?
-      debug 7, "get local and push"
+      debug 7, "Git::get_branch get local and push"
       self.push("origin",branch)
     elsif remote.any?
-      debug 7, "get remote"
+      debug 7, "Git::get_branch get remote"
       self.config("remote.#{branch}.remote", "origin")
       self.config("remote.#{branch}.merge", "refs/heads/#{branch}")
       self.checkout("origin/#{branch}", :new_branch => branch)
-      self.pull("origin")
+      self.pull("origin",branch)
     else
-      debug 7, "new branch"
+      debug 7, "Git::get_branch new branch"
       self.checkout("zero",:new_branch => branch)
       self.config("remote.#{branch}.remote", "origin")
       self.config("remote.#{branch}.merge", "refs/heads/#{branch}")
-      self.push("origin")
+      self.push("origin",branch)
     end
+  end
+
+  # Now just push everything in this branch and make sure it goes.
+  def push_branch(branch = self.current_branch,*opts)
+    debug 7, "Git::push_branch #{branch} opts=#{opts}"
+    # really want to be able to add :force=>true...
+    self.push("origin",branch)
   end
 end
 
